db3e8310f951a521d4a7025ea03b35c06d718196
吴浩渺 <wuhaomiao@wuhaomiaos-MacBook-Pro.local>
2019-08-06 21:31:23 +0800

    fix bug in loading data

--- feature.py
+++ feature.py
@@ -1,7 +1,6 @@
 import h5py
 import time
 import numpy as np
-from sklearn import decomposition
 from sklearn import preprocessing
 from sklearn.externals import joblib
 N = 1005
@@ -17,15 +16,15 @@ print('Loading PCA complete. Time used: ', time.time() - start)
 start = time.time()
 scaler = preprocessing.StandardScaler()
 with h5py.File('train.h5', 'r') as ipt:
-    with h5py.File('train_feature.h5', 'r') as opt:
-        while n <= 9000:
+    with h5py.File('train_feature.h5', 'w') as opt:
+        while n <= 1000:
+            print(f'Loading data part {part}...')
             for i in range(n-1000, n):
-                print(f'Loading data part {part}...')
                 start = time.time()
                 sample = ipt[f'{i:04d}']['QPI'][...]
                 sample = sample.reshape((1, N2))
                 data[i] = sample
-                print('Loading complete. Time used: ', time.time() - start)
+            print('Loading complete. Time used: ', time.time() - start)
             print(f'Scaling data part {part}...')
             start = time.time()
             scaler.fit(data)
